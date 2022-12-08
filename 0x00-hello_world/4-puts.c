39956d0925211defc14ae0ca4ee4b82e3fc434b4
Ebiwari <ebiwari@gmail.com>
2022-12-08 10:42:24 -0800

    Workgin of on the lenght of task 4 c programing

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
+/**
+ * MAIN METHOD
+ * USING PUT  METTHOD
+ *
+ */
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle)
 	return(0);
 }
