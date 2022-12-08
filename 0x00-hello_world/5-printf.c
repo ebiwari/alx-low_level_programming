9d8b9df5d2e5131ec43a4251efb3bc2d8f9efa24
Ebiwari <ebiwari@gmail.com>
2022-12-08 10:18:24 -0800

    Added commnet fot the  main function

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
-
+/**
+ * main-Entry
+ * Return : 0 after printing to the console
+ *
+ *
+ */
 
 int main(void)
 {
-	printf("with proper grammer, but the outcome is a piece of art,\n");
+	printf ("with proper grammer, but the outcome is a piece of art,\n");
 	return(0);
 
 }
