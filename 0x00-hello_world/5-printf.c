3d48131858235a1a7fb543d41abb7d948e07eee5
Ebiwari <ebiwari@gmail.com>
2022-12-11 07:24:28 -0800

    r

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 /**
- * main-Entry
- * Return : 0 after printing to the console
+ * main - Entry
  *
- *
+ * Return : 0 
  */
-
+  
 int main(void)
 {
-	printf ("with proper grammer, but the outcome is a piece of art, \n");
-	return(0);
+	printf ("with proper grammar, but the outcome is a piece of art, \n");
+	return (0);
 
 }
