78c28875ff0f8637aa5502b7a0360e90cb4a50b3
Ebiwari <ebiwari@gmail.com>
2022-12-11 07:12:20 -0800

    cm

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 /**
  * main - Makes the sum of two numbers
@@ -9,5 +10,4 @@ int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-			i
 }
