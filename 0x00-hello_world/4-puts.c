8315b52b67f2a88a5d60d1f61be161864d7c13ef
Ebiwari <ebiwari@gmail.com>
2022-12-11 00:52:38 -0800

    update betty 1.0 for c-4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
-/*
- * main - output a line of code using puts
- * Return:0
- *
+/**
+ * Main - Makes the sum of two numbers
+ * Description: Using the puts function to return a string.
+ * Return: 0
  */
 
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
+			i
 }
