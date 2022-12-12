8918a6520d9ae225ddeb3b3a929e211f77ad5466
Ebiwari <ebiwari@gmail.com>
2022-12-12 15:43:20 -0800

    variable task1 upload

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry
+ * Description: A program that us printf
+ * Return: 0
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
