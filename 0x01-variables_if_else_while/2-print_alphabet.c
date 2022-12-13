e675fafd5fde37ce7ad04b243880fd0d598f026e
Ebiwari <ebiwari@gmail.com>
2022-12-12 16:13:29 -0800

    Task variable -1

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,13 +1,34 @@
 #include <stdio.h>
-
+#include <stdlib.h>
+#include <time.h>
 /**
  * main - Entry
- * Description: A program that us printf
+ * Description: A program to print if number + - or 0
  * Return: 0
 */
 
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
+	int n;
+	int a;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	a = n % 10;
+
+	if (a > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, a);
+	}
+	else if (a == 0)
+	{
+		printf("Last digit of %d is %d and is 0\n", n, a);
+	}
+	else
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, a);
+
+	}
+
 	return (0);
 }
