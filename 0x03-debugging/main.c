a72cfb12213af3cc3f3e73c27398aa345aa26c82
Ebiwari <ebiwari@gmail.com>
2022-12-15 04:31:53 -0800

    Debugging intial commit

---
+++ 0x03-debugging/main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - test function that print if integers is positive or negative
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 98;
+	positive_or_negative(i);
+	
+	return (0);
+
+}
